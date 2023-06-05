28eeb4e26d63a55d917c1a4d8400b7a9255ff3cf
ikeejames <ikee.james@gmail.com>
2023-06-05 09:27:56 +0800

    update app.jsx client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Header from './components/Header'
-import ShoppingForm from './components/ShoppingForm'
 import TodoForm from './components/TodoForm'
 import './App.css'
 import './sass/main.scss'
