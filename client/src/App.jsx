1b4a249895c57c0b6d4052d24fdec6916132e699
ikeejames <ikee.james@gmail.com>
2023-06-05 11:46:08 +0800

    added footer client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import TodoForm from './components/TodoForm'
 import './App.css'
 import './sass/main.scss'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
       <div className='container'>
         <Header />
         <TodoForm />
+        <Footer />
       </div>
     </>
   )
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>my sample project</p>
+            <p> <span>&copy;</span> ikee 2023</p>
+        </div>
+    )
+}
+
+export default Footer;
